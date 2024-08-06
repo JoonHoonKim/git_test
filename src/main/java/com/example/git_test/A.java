158d521584d48302ad5b591516344970753c2a46
JoonHoonKim <wnsgns05058@naver.com>
2024-08-06 15:12:31 +0900

    A

--- src/main/java/com/example/git_test/A.java
+++ src/main/java/com/example/git_test/A.java
@@ -3,5 +3,8 @@ package com.example.git_test;
 public class A {
     public void methodA() {
         System.out.println("A");
+        System.out.println("A");
+
+        System.out.println("B");
     }
 }
